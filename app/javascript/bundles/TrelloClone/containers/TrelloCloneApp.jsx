f66429188eca927fba0179805ca21e68b45a9d2f
Akhil Stanislavose <akhil.stanislavose@gmail.com>
2017-10-06 18:07:08 -0700

    More cleanup

--- app/javascript/bundles/TrelloClone/containers/TrelloCloneApp.jsx
+++ app/javascript/bundles/TrelloClone/containers/TrelloCloneApp.jsx
@@ -8,16 +8,9 @@ const mapStateToProps = (state) => {
 
   return({
     ...state.boardsById.get(defaultBoardId).toJS(),
-    decks: state.decksById.filter((deck,_) => deck.get('boardId') === defaultBoardId).valueSeq().toJS()
   });
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return({
-    createDeck: (deck) => dispatch(deckActions.createDeck(deck))
-  });
-};
-
-const TrelloCloneApp = connect(mapStateToProps, mapDispatchToProps)(Board);
+const TrelloCloneApp = connect(mapStateToProps, null)(Board);
 
 export default TrelloCloneApp;
